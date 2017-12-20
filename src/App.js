419e61d75ea337086c38efb6cb34d1dc5f52f9b9
mrjasonweaver <indyplanets@gmail.com>
2017-12-20 14:10:07 -0600

    fetch repo issues, set state and display to issue list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+const root = 'https://api.github.com';
+let username = 'facebook';
+let repo = 'react'; 
+let params = `${username}/${repo}`;
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      issues: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(`${root}/repos/${params}/issues`)
+      .then(response => response.json())
+      .then(data => {
+        let issues = data.map(issue => {
+          return(
+            <li key={issue.id}  className={`id-${issue.id}`}>
+              <span>{issue.title}</span>
+            </li>
+          )
+        });
+        this.setState({issues: issues});
+        console.log("state", this.state.issues);
+      }).catch( error => console.error(error));
+  }
+
   render() {
     return (
       <MuiThemeProvider>
@@ -21,9 +47,9 @@ class App extends Component {
             />
             <RaisedButton label="Get issues" primary={true} />
           </header>
-          <p className="App-intro">
-            Issues here
-          </p>
+          <ul className="issue-list">
+           {this.state.issues}
+          </ul>
         </div>
       </MuiThemeProvider>
     );
