22f8c4219b0f209d9f7074418f496376d3b64f8f
mrjasonweaver <indyplanets@gmail.com>
2018-03-05 05:58:16 -0600

    Adding Link router component for list item

--- src/RepoIssues/components/IssueList.jsx
+++ src/RepoIssues/components/IssueList.jsx
@@ -6,6 +6,7 @@ import CircularProgress from 'material-ui/CircularProgress';
 import RadioButtonChecked from 'material-ui/svg-icons/toggle/radio-button-checked';
 import RadioButtonUnchecked from 'material-ui/svg-icons/toggle/radio-button-unchecked';
 import {cyan400, grey200} from 'material-ui/styles/colors';
+import {Link} from 'react-router-dom';
 
 const IssueList = props => {
   const {issues, loading, selectedIssueUrl, onIssueSelect } = props;
@@ -14,22 +15,24 @@ const IssueList = props => {
     return (
       <div className="issueContainer">
         {issues.map(issue =>
-          <div 
+          <div
             key={issue.id} 
             data-htmlurl={issue.html_url}
             data-issueid={issue.id}
             data-issuedata={JSON.stringify(issue.issueData)}
             onClick={onIssueSelect}>
-            <ListItem
-              className={`id-${issue.id}`}
-              primaryText={issue.title}
-              secondaryText={
-                <p>{issue.user_login} -- {issue.comments} Comments</p>
-              }
-              secondaryTextLines={1}
-              rightAvatar={<Avatar src={issue.user_avatar_url} />}
-              leftIcon={selectedIssueUrl === issue.html_url ? <RadioButtonChecked color={cyan400} /> : <RadioButtonUnchecked color={grey200} />}>
-            </ListItem>
+            <Link to="/" style={{textDecoration: "none"}}>
+              <ListItem
+                className={`id-${issue.id}`}
+                primaryText={issue.title}
+                secondaryText={
+                  <p>{issue.user_login} -- {issue.comments} Comments</p>
+                }
+                secondaryTextLines={1}
+                rightAvatar={<Avatar src={issue.user_avatar_url} />}
+                leftIcon={selectedIssueUrl === issue.html_url ? <RadioButtonChecked color={cyan400} /> : <RadioButtonUnchecked color={grey200} />}>
+              </ListItem>
+            </Link>
             <Divider />
           </div>
         )}
